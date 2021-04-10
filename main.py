17e6479f60f70d940ba64bac7f83e4263db38ecc
abdul rehman <ar.rehmanmirza@gmail.com>
2021-04-10 18:43:16 +0500

    save button

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import base64
 import json
 import os
 from src.Detection.detction import main_detection
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from werkzeug.utils import secure_filename
 
 from src.database.db import init_db, db_session
 from flask_cors import cross_origin
 from src.models.user import User
 
-app = Flask(__name__, instance_relative_config=True,static_folder="static/dist",template_folder="static")
+app = Flask(__name__, instance_relative_config=True,static_folder="build",template_folder="template")
 # app.config.from_mapping(
 #     SECRET_KEY = 'DEV')
 # import os
@@ -25,7 +25,6 @@ def upload_image():
   destination = "/".join([target, filename])
   file.save(destination)
   res = main_detection(filename)
-  print(res)
   user = User(img=filename, result_number=res)
   db_session.add(user)
   db_session.commit()
@@ -33,8 +32,21 @@ def upload_image():
       "message":"Successfully Uploaded Image",
       "result": res
   }
-
   return json.dumps(returnData)
+
+@app.route("/api/save_text", methods=['GET'])
+@cross_origin()
+def save_text():
+  # record = json.loads(request.data)
+  res = request.args.get("text")
+  # res = record['text']
+  number_save = open("build/number.txt", "a")
+  number_save.writelines("number= " + res + "\n")
+  number_save.close()
+  returnData = {
+      "message": "Successfully save number in file"
+  }
+  return send_from_directory(app.static_folder,"number.txt",as_attachment=True )
 
 if __name__ == '__main__':
     init_db()
